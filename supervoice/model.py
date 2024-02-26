ref: working training, fixing phoneme tokenizer, fixing masking to avoid nans in transformers, generator and prediction of next token, sampling tokenizing

supervoice/model.py
@@ -1,21 +1,22 @@
 import torch
 from torch.nn import functional as F
 from .transformer import Transformer, TransformerAdvanced
+from .masks import create_padding_mask, create_padding_casual_mask, create_padding_rectangle_mask
 
 class SupervoiceGPT(torch.nn.Module):
     def __init__(self, config):
         super(SupervoiceGPT, self).__init__()
         self.config = config
         self.n_input_tokens = config.tokenizer.vocab_size
-        self.n_output_tokens = config.tokenizer.vocab_size_output
-        self.n_durations = config.gpt.max_durations - config.gpt.min_durations + 1
+        self.n_output_tokens = len(config.tokenizer.vocab_output)
+        self.n_durations = (config.gpt.max_durations - config.gpt.min_durations + 1) + 1 # +1 Padding
 
         # Embeddings
         self.input_embedding = torch.nn.Embedding(self.n_input_tokens, self.config.gpt.n_dim)
         torch.nn.init.normal_(self.input_embedding.weight, mean=0.0, std=0.02)
-        self.output_embedding_token = torch.nn.Embedding(self.n_output_tokens, self.config.gpt.n_dim)
+        self.output_embedding_token = torch.nn.Embedding(self.n_output_tokens, self.config.gpt.n_dim - self.config.gpt.n_dim_duration)
         torch.nn.init.normal_(self.output_embedding_token.weight, mean=0.0, std=0.02)
-        self.output_embedding_duration = torch.nn.Embedding(self.n_durations, self.config.gpt.n_dim)
+        self.output_embedding_duration = torch.nn.Embedding(self.n_durations, self.config.gpt.n_dim_duration)
         torch.nn.init.normal_(self.output_embedding_duration.weight, mean=0.0, std=0.02)
 
         # Encoder Transformer
@@ -55,8 +56,8 @@ class SupervoiceGPT(torch.nn.Module):
         )
 
         # Prediction heads
-        self.prediction_head_token = torch.nn.Linear(self.config.gpt.n_dim, self.n_output_tokens, bias=False)
-        self.prediction_head_duration = torch.nn.Linear(self.config.gpt.n_dim, self.n_durations, bias=False)
+        self.prediction_head_token = torch.nn.Linear(self.config.gpt.n_dim - self.config.gpt.n_dim_duration, self.n_output_tokens, bias=False)
+        self.prediction_head_duration = torch.nn.Linear(self.config.gpt.n_dim_duration, self.n_durations, bias=False)
 
         # Weight sharing
         self.output_embedding_token.weight = self.prediction_head_token.weight
@@ -65,106 +66,136 @@ class SupervoiceGPT(torch.nn.Module):
     def forward(self, *,
         input, 
         input_lengths = None, 
-        output, 
+        output_tokens, 
+        output_durations,
         output_lengths = None, 
-        target_phonemes = None, 
+        target_tokens = None, 
         target_durations = None
     ):
 
-        # Create input mask for cross-attention which is useful for training on variable length sequences
-        input_mask = None
-        if input_lengths is not None:
-            input_mask = padded_square_mask(input_lengths, input.size(1), casual = False, device = input.device)
+        # Check input
+        assert len(input.size()) == 2, 'Input tensor shape should be [batch_size, sequence_length]'
+        assert len(output_tokens.size()) == 2, 'Input tensor shape should be [batch_size, sequence_length]'
+        assert len(output_durations.size()) == 2, 'Input tensor shape should be [batch_size, sequence_length]'
+        assert input.size(0) == output_tokens.size(0), 'Input and output batch size should be the same'
+        assert output_tokens.size(0) == output_durations.size(0), 'Output batch sizes should be the same'
+        assert output_tokens.size(1) == output_durations.size(1), 'Output sequence lengths should be the same'
+
+        # Create input mask for self-attention which is useful for training on variable length sequences
+        if input_lengths is None:
+            input_lengths = torch.tensor([input.size(1)] * input.size(0), device = input.device)
+        input_mask = create_padding_mask(input_lengths, input.size(1), device = input.device).unsqueeze(1)
 
         # Create output mask for self-attention which is useful for training on variable length sequences
-        output_mask = None
-        if output_lengths is not None:
-            output_mask = padded_square_mask(output_lengths, output.size(1), casual = True, device = input.device)
+        if output_lengths is None:
+            output_lengths = torch.tensor([output_tokens.size(1)] * output_tokens.size(0), device = output_tokens.device)
+        output_mask = create_padding_casual_mask(output_lengths, output_tokens.size(1), device = output_tokens.device).unsqueeze(1)
 
-        # Input embeddings
+        # Create input-output masks for cross-attention which is useful for training on variable length sequences
+        input_output_mask = create_padding_rectangle_mask(output_lengths, input_lengths, output_tokens.size(1), input.size(1), device = input.device).unsqueeze(1)
+
+        # Embeddings
         input_embedded = self.input_embedding(input)
-
-        # Output embeddings
-        output_embedded = self.output_embedding_token(output)
+        output_tokens_embedded = self.output_embedding_token(output_tokens)
+        output_durations_embedded = self.output_embedding_duration(output_durations)
+        output_embedded = torch.cat((output_tokens_embedded, output_durations_embedded), dim = -1)
 
         # Run an encoder
         latents = self.encoder(input_embedded, mask = input_mask)
 
         # Run an decoder
-        decoded = self.decoder(latents, output_embedded, x_mask = input_mask, y_mask = output_mask)
+        decoded = self.decoder(latents, output_embedded, x_mask = input_mask, y_mask = output_mask, xy_mask = input_output_mask)
+
+        # Split the output into token and duration
+        decoded_token = decoded[:, :, :self.config.gpt.n_dim - self.config.gpt.n_dim_duration]
+        decoded_duration = decoded[:, :, self.config.gpt.n_dim - self.config.gpt.n_dim_duration:]
 
         # Run prediction head
-        predicted_token = self.prediction_head_token(decoded)
-        preducted_duration = self.prediction_head_duration(decoded)
+        predicted_token = self.prediction_head_token(decoded_token)
+        predicted_duration = self.prediction_head_duration(decoded_duration)
 
         # Compute loss if targets are provided
-        if target_phonemes is not None and target_durations is not None:
-            loss_duration = F.cross_entropy(preducted_duration.view(-1, preducted_duration.size(-1)), target_durations.view(-1), ignore_index = -1)
-            loss_token = F.cross_entropy(predicted_token.view(-1, predicted_token.size(-1)), target_phonemes.view(-1), ignore_index = -1)
+        if target_tokens is not None and target_durations is not None:
+            loss_duration = F.cross_entropy(predicted_duration.view(-1, predicted_duration.size(-1)), target_durations.view(-1), ignore_index = 0)
+            loss_token = F.cross_entropy(predicted_token.view(-1, predicted_token.size(-1)), target_tokens.view(-1), ignore_index = 0)
             loss = loss_token + loss_duration
-            return predicted_token, preducted_duration, loss
+            return predicted_token, predicted_duration, loss
 
-        return predicted_token, preducted_duration
+        return predicted_token, predicted_duration
 
-    # @torch.no_grad()
-    # def generate(self, input, max_new_tokens, temperature=1.0, top_k=None, stop_tokens = None, deterministic = False):
-    #     ctx = input
-    #     for _ in range(max_new_tokens):
+    @torch.no_grad()
+    def generate(self, input, tokenizer, max_new_tokens = 128, temperature=1.0, top_k=None, deterministic = False, device="cpu"):
+        ctx_input = torch.tensor([tokenizer.sequence_begin_token_id] + tokenizer.encode(input) + [tokenizer.sequence_end_token_id], device = device).unsqueeze(0)
+        ctx_output_tokens = torch.tensor([tokenizer.sequence_begin_token_id], device = device).unsqueeze(0)
+        ctx_output_durations = torch.tensor([0], device = device).unsqueeze(0)
+        valid_exit = False
+        for _ in range(max_new_tokens):
             
-    #         # Forward the model to get the logits for the index in the sequence
-    #         logits = self(ctx)
+            # Forward the model to get the logits for the index in the sequence
+            logits_token, logits_duration = self(input = ctx_input, output_tokens = ctx_output_tokens, output_durations = ctx_output_durations)
             
-    #         # Pluck the logits at the final step and scale by desired temperature
-    #         logits = logits[:, -1, :] / temperature
+            # Pluck the logits at the final step and scale by desired temperature
+            logits_token = logits_token[:, -1, :] / temperature
+            logits_duration = logits_duration[:, -1, :] / temperature
 
-    #         # Truncate the logits to only having generate tokens
-    #         # logits = logits[:, :self.n_generate_tokens]
+            # Truncate the logits to only having generate tokens
+            # logits = logits[:, :self.n_generate_tokens]
             
-    #         # Optionally crop the logits to only the top k options
-    #         if top_k is not None:
-    #             v, _ = torch.topk(logits, min(top_k, logits.size(-1)))
-    #             logits[logits < v[:, [-1]]] = -float('Inf')
+            # Optionally crop the logits to only the top k options
+            if top_k is not None:
+                v, _ = torch.topk(logits_token, min(top_k, logits_token.size(-1)))
+                logits_token[logits_token < v[:, [-1]]] = -float('Inf')
+                v, _ = torch.topk(logits_duration, min(top_k, logits_duration.size(-1)))
+                logits_duration[logits_duration < v[:, [-1]]] = -float('Inf')
             
-    #         # Apply softmax to convert logits to (normalized) probabilities
-    #         probs = F.softmax(logits, dim=-1)
+            # Apply softmax to convert logits to (normalized) probabilities
+            probs_token = F.softmax(logits_token, dim=-1)
+            probs_duration = F.softmax(logits_duration, dim=-1)
             
-    #         # Sample from the distribution
-    #         if deterministic:
-    #             idx_next = torch.argmax(probs, dim=-1, keepdim=True)
-    #         else:
-    #             idx_next = torch.multinomial(probs, num_samples=1)
+            # Sample from the distribution
+            if deterministic:
+                idx_next_token = torch.argmax(probs_token, dim=-1, keepdim=True)
+                idx_next_duration = torch.argmax(probs_duration, dim=-1, keepdim=True)
+            else:
+                idx_next_token = torch.multinomial(probs_token, num_samples=1)
+                idx_next_duration = torch.multinomial(probs_duration, num_samples=1)
             
-    #         # Append Context
-    #         ctx = torch.cat((ctx, idx_next), dim=1)
+            # Append Context
+            ctx_output_tokens = torch.cat((ctx_output_tokens, idx_next_token), dim=1)
+            ctx_output_durations = torch.cat((ctx_output_durations, idx_next_duration), dim=1)
 
-    #         # Stop Tokens
-    #         if idx_next in stop_tokens:
-    #             break
+            # Stop Tokens
+            if idx_next_token == tokenizer.sequence_end_token_id:
+                valid_exit = True
+                break
 
-    #     return ctx
+        tokens = tokenizer.decode_phonemes(ctx_output_tokens.squeeze(0).cpu().tolist())
+        durations = (ctx_output_durations.squeeze(0).cpu() - 1).tolist()
+        tokens = tokens[1:]
+        durations = durations[1:]
+        if valid_exit:
+            tokens = tokens[:-1]
+            durations = durations[:-1]
+        return tokens, durations
 
-    # def predict_next(self, input, top_k, trim_generated = True):
+    def predict_next(self, input, output_tokens, output_durations, tokenizer, top_k = 10, device = "cpu"):
 
-    #     # Predict next token
-    #     logits = self(input.unsqueeze(0)).squeeze(0)
-    #     logits = logits[-1, :]
-    #     if trim_generated:
-    #         logits = logits[:self.n_generate_tokens]
+        # Context
+        ctx_input = torch.tensor([tokenizer.sequence_begin_token_id] + tokenizer.encode(input) + [tokenizer.sequence_end_token_id], device = device).unsqueeze(0)
+        ctx_output_tokens = torch.tensor([tokenizer.sequence_begin_token_id] + tokenizer.encode_phonemes(output_tokens), device = device).unsqueeze(0)
+        ctx_output_durations = torch.tensor([0] + output_durations, device = device).unsqueeze(0)
 
-    #     # Probabilities
-    #     probs = F.softmax(logits, dim=-1)
+        # Predict next token
+        logits_token, logits_duration = self(input = ctx_input, output_tokens = ctx_output_tokens, output_durations = ctx_output_durations)
+        logits_token.squeeze_(0)
+        logits_duration.squeeze_(0)
+        logits_token = logits_token[-1, :]
+        logits_duration = logits_duration[-1, :]
 
-    #     # Get top k
-    #     probs, indices = torch.topk(probs, top_k)
+        # Probabilities
+        probs_token = F.softmax(logits_token, dim=-1)
+
+        # Get top k
+        probs_token, indices = torch.topk(probs_token, top_k)
         
-    #     return probs, indices
-
-def padded_square_mask(lengths, max_length, casual, device):
-    batch_size = lengths.size(0)
-    mask = torch.zeros(batch_size, max_length, max_length, device = device, dtype = torch.bool)
-    for i in range(batch_size):
-        mask[i, :lengths[i], :lengths[i]] = True
-    mask = torch.where(mask, 0, float('-inf'))
-    if casual:
-        mask = mask + torch.triu(torch.full((max_length, max_length), float('-inf'), device = device), diagonal = 1)
-    return mask
+        return probs_token.cpu().tolist(), tokenizer.decode_phonemes(indices.cpu().tolist())

supervoice/transformer.py
@@ -142,7 +142,7 @@ class TransformerAdvanced(nn.Module):
             raise ValueError(f"Unknown position embedding: {position_embedding}")
 
 
-    def forward(self, x, y, x_mask = None, y_mask = None):
+    def forward(self, x, y, x_mask = None, y_mask = None, xy_mask = None):
         batch, seq_len_x, *_ = x.shape
         _, seq_len_y, *_ = y.shape
 
@@ -157,7 +157,7 @@ class TransformerAdvanced(nn.Module):
         # Inspired by: https://github.com/ofirpress/attention_with_linear_biases/issues/5
         if self.position_embedding == 'alibi':
             alibi_x = get_alibi_mask(seq_len_x, self.n_heads, False, x.device)
-            alibi_y = get_alibi_mask(seq_len_y, self.n_heads, False, x.device)
+            alibi_y = get_alibi_mask(seq_len_y, self.n_heads, False, y.device)
 
         # Compute rotary embeddings
         if self.position_embedding == 'rotary':
@@ -176,7 +176,19 @@ class TransformerAdvanced(nn.Module):
         # Run through decoder attention blocks
         decoder_output = y
         for decoder in self.layers_dec:
-            decoder_output = decoder(decoder_output, encoder_output, alibi = alibi_y, rotational = rotational_y, self_mask = y_mask, cross_mask = x_mask)
+            decoder_output = decoder(
+
+                # Self atention for decoder outputs
+                x = decoder_output,
+                self_alibi = alibi_y, 
+                self_rotational = rotational_y, 
+                self_mask = y_mask, 
+
+                # Cross attention between encoder and decoder outputs                
+                y = encoder_output,
+                cross_mask = xy_mask,
+                # NOTE: No positional encodings are used in cross attention
+            )
 
         # Output normalization
         decoder_output = self.output_norm(decoder_output)
@@ -238,19 +250,19 @@ class AttentionBlockAdvanced(torch.nn.Module):
         self.mlp_ln = RMSNorm(n_dim)
         self.mlp = AttentionMLP(n_dim, n_dim_ffn, ffn_dropout)
 
-    def forward(self, x, y, alibi = None, rotational = None, self_mask = None, cross_mask = None):
+    def forward(self, x, y, self_alibi = None, cross_alibi = None, self_rotational = None, cross_rotational = None, self_mask = None, cross_mask = None):
 
         # Self Attention
         residual = x
         x = self.attention_self_ln(x)
-        x = self.attention_self(x_q = x, x_k = x, x_v = x, alibi = alibi, rotational = rotational, mask = self_mask)
+        x = self.attention_self(x_q = x, x_k = x, x_v = x, alibi = self_alibi, rotational = self_rotational, mask = self_mask)
         x = x + residual
 
         # Cross Attention
         residual = x
         x = self.attention_cross_ln_x(x)
         y = self.attention_cross_ln_y(y)
-        x = self.attention_cross(x_q = x, x_k = y, x_v = y, alibi = alibi, rotational = rotational, mask = cross_mask)
+        x = self.attention_cross(x_q = x, x_k = y, x_v = y, alibi = cross_alibi, rotational = cross_rotational, mask = cross_mask)
         x = x + residual
 
         # MLP
@@ -296,7 +308,6 @@ class Attention(torch.nn.Module):
         q = self._split_heads(self.attention_q(x_q))
         k = self._split_heads(self.attention_k(x_k))
         v = self._split_heads(self.attention_v(x_v))
-        # q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h = self.n_heads), (q, k, v))
 
         # Rotary embedding
         if rotational is not None:
@@ -312,14 +323,6 @@ class Attention(torch.nn.Module):
                 target_mask = alibi
 
         # Dot product attention
-        # print(
-        #     q.shape, 
-        #     k.shape,
-        #     v.shape, 
-        #     target_mask.shape if target_mask is not None else None, 
-        #     mask.shape if mask is not None else None,
-        #     alibi.shape if alibi is not None else None,
-        # )
         y = torch.nn.functional.scaled_dot_product_attention(q, k, v, 
             attn_mask = target_mask, 
             dropout_p = self.att_dropout if self.training else 0.0, 

train.py
@@ -28,13 +28,14 @@ from accelerate.utils import set_seed
 from train_config import config
 from utils.datasets import create_dataset_loader
 from supervoice.model import SupervoiceGPT
+from supervoice.tokenizer import Tokenizer
 
 # Train parameters
-train_experiment = "gpt_new_tokenizer_2"
+train_experiment = "enc_dec_fix_phonemes"
 train_project="supervoice-gpt"
 train_auto_resume = True
-train_batch_size = 16 # Per GPU
-train_sequence_length = 2048
+train_batch_size = 72 # Per GPU
+train_sequence_length = 256
 train_grad_accum_every = 8
 train_steps = 600000
 train_loader_workers = 8
@@ -65,10 +66,12 @@ def main():
     set_seed(42)
     lr_start = train_lr_start * accelerator.num_processes
     lr_max = train_lr_max * accelerator.num_processes
+    # torch.set_printoptions(profile="full")
 
     # Prepare dataset
     accelerator.print("Loading dataset...")
-    train_loader = create_dataset_loader("./datasets/train.bin", batch_size = train_batch_size, sequence_length = train_sequence_length, workers = train_loader_workers, dtype = dtype)
+    tokenizer = Tokenizer(config, "./tokenizer_text.model")
+    train_loader = create_dataset_loader("./datasets/train.txt", batch_size = train_batch_size, sequence_length = train_sequence_length, workers = train_loader_workers, tokenizer = tokenizer)
 
     # Model
     accelerator.print("Loading model...")
@@ -157,10 +160,25 @@ def main():
                 with accelerator.autocast():
 
                     # Load batch
-                    source, target = next(train_cycle)
+                    x, x_lengths, y_p, y_d, y_lengths, t_p, t_d = next(train_cycle)
+                    # print("inputs")
+                    # print(t_p)
+                    # print(t_d)
 
                     # Forward
-                    predicted, loss = model(source, target)
+                    predicted_t, predicted_d, loss = model(
+                        input = x, 
+                        input_lengths = x_lengths,
+                        output_tokens = y_p,
+                        output_durations = y_d,
+                        output_lengths = y_lengths,
+                        target_tokens = t_p,
+                        target_durations = t_d
+                    )
+
+                    # if torch.isnan(loss).any():
+                    #     accelerator.print("NaN detected")
+                    #     raise Exception("NaN detected")
 
                     # Backprop
                     optim.zero_grad()

utils/datasets.py
@@ -6,24 +6,39 @@ import os
 def create_dataset_loader(path, sequence_length, batch_size, tokenizer, workers):
 
     # Load dataset
-    dataset = PhonemesDataset(path, PhonemesDataset)
+    dataset = PhonemesDataset(path, tokenizer, sequence_length)
 
     # Collator
     def collate_fn(batch):
+        B = len(batch)
         x, y_d, y_p = zip(*batch)
 
         # Calculate lengths
         x_lengths = torch.tensor([len(x) for x in x])
-        y_d_lengths = torch.tensor([len(y) for y in y_d])
-        y_p_lengths = torch.tensor([len(y) for y in y_p])
+        y_lengths = torch.tensor([len(y) - 1 for y in y_d])
 
-        # Pad sequences
-        # x = torch.nn.utils.rnn.pad_sequence(x, batch_first=True, padding_value=tokenizer.pad_token_id)
-        # y_d = torch.nn.utils.rnn.pad_sequence(y_d, batch_first=True, padding_value=0)
-        # y_p = torch.nn.utils.rnn.pad_sequence(y_p, batch_first=True, padding_value=tokenizer.pad_token_id)
-        return x, x_lengths, y_d, y_d_lengths, y_p, y_p_lengths
+        # Create targets
+        t_d = torch.zeros(B, sequence_length, dtype = torch.int64)
+        t_p = torch.zeros(B, sequence_length, dtype = torch.int64)
+        for i in range(B):
+            t_d[i, :len(y_d[i]) - 1] = y_d[i][1:]
+            t_p[i, :len(y_p[i]) - 1] = y_p[i][1:]
 
-    loader = torch.utils.data.DataLoader(dataset, batch_size = batch_size, num_workers = workers, shuffle=False, pin_memory=True, drop_last=True)
+        # Padded tensors
+        x_padded = torch.IntTensor(B, sequence_length)
+        y_t_padded = torch.IntTensor(B, sequence_length)
+        y_d_padded = torch.IntTensor(B, sequence_length)
+        x_padded.zero_()
+        y_t_padded.zero_()
+        y_d_padded.zero_()
+        for i in range(B):
+            x_padded[i, :len(x[i])] = x[i]
+            y_t_padded[i, :len(y_p[i]) - 1] = y_p[i][:-1]
+            y_d_padded[i, :len(y_d[i]) - 1] = y_d[i][:-1]
+
+        return x_padded, x_lengths, y_t_padded, y_d_padded, y_lengths, t_p, t_d
+
+    loader = torch.utils.data.DataLoader(dataset, batch_size = batch_size, num_workers = workers, shuffle=False, pin_memory=True, drop_last=True, collate_fn = collate_fn)
     return loader
 
 #
@@ -50,8 +65,10 @@ class GPTDataset(torch.utils.data.IterableDataset):
         return iter(self.generate())
 
 class PhonemesDataset(torch.utils.data.IterableDataset):
-    def __init(self, path, tokenizer):
+    def __init__(self, path, tokenizer, max_sequence):
         self.tokenizer = tokenizer
+        self.data = []
+        self.max_sequence = max_sequence
         with open(path, 'r') as file:
             for line in file.readlines():
                 text, phonemes = line.split('｜')
@@ -69,13 +86,18 @@ class PhonemesDataset(torch.utils.data.IterableDataset):
             text, phonemes = item
             
             # Prepare input
-            x = torch.tensor(self.tokenizer.encode(text)).long()
+            input_tokens = self.tokenizer.encode(text) if random.random() < 0.3 else self.tokenizer.encode_sample(text) # 30% chance of using optimal tokenization
+            x = torch.tensor([self.tokenizer.sequence_begin_token_id] + input_tokens + [self.tokenizer.sequence_end_token_id]).int()
 
             # Prepare output
-            y_d = torch.tensor([d for _, d in phonemes]).long()
-            y_p = torch.tensor(self.tokenizer.encode_phoneme([p for p, _ in phonemes])).long()
+            y_p = torch.tensor([self.tokenizer.sequence_begin_token_id] + self.tokenizer.encode_phonemes([p for p, _ in phonemes]) + [self.tokenizer.sequence_end_token_id]).int()
+            y_d = torch.tensor([0] + [d + 1 for _, d in phonemes] + [0]).int()
 
-            return (x, y_d, y_p)
+            # Check if sequence is too long
+            if len(x) > self.max_sequence or len(y_p) > self.max_sequence:
+                continue
+
+            yield (x, y_d, y_p)
     
     def __iter__(self):
         return iter(self.generate())