895dbd37009a079d542a7f081ed6233f2659e059
Steve Korshakov <steve@korshakov.com>
2024-02-27 12:39:07 +0000

    chore: download tokenizer

--- hubconf.py
+++ hubconf.py
@@ -7,12 +7,12 @@ def supervoice():
     from supervoice_gpt import SupervoiceGPT, Tokenizer, config
 
     # Download
-    # hub_dir = torch.hub.get_dir()
-    # model_dir = os.path.join(hub_dir, 'checkpoints', 'supervoice_tokenizer.model')
-    # torch.hub.download_url_to_file("https://shared.korshakov.com/models/supervoice_tokenizer.model", model_dir)
+    hub_dir = torch.hub.get_dir()
+    model_dir = os.path.join(hub_dir, 'checkpoints', 'supervoice-gpt-1-tokenizer.model')
+    torch.hub.download_url_to_file("https://shared.korshakov.com/models/supervoice-gpt-1-tokenizer.model", model_dir)
 
     # Tokenizer
-    tokenizer = Tokenizer(config, "tokenizer_text.model")
+    tokenizer = Tokenizer(config, model_dir)
 
     # Model
     model = SupervoiceGPT(config)
