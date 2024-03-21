5c316bdbc7c70164ac4fe9a9a826976c4f546b0d
Steve Korshakov <steve@korshakov.com>
2024-03-21 04:45:53 +0000

    fix: fix torch hub

--- hubconf.py
+++ hubconf.py
@@ -9,7 +9,7 @@ def phonemizer():
 
     # Model
     tokenizer = Tokenizer(config, os.path.join(os.path.dirname(__file__), "tokenizer_text.model"))
-    model = SupervoiceGPT(config, tokenizer)
+    model = SupervoiceGPT(tokenizer, config)
     checkpoint = torch.hub.load_state_dict_from_url("https://github.com/ex3ndr/supervoice-gpt/releases/download/v0.0.1/supervoice_gpt_pitch_255000.pt", map_location="cpu")
     model.load_state_dict(checkpoint['model'])
     model.eval()
